7a7180012c89db94261950a68a9d13a0a0788458
bbongcat <lollolllo328@gmail.com>
2021-04-06 12:00:34 +0900

    [21.04.06] # 게시판 CRUD 학습

---
+++ src/main/java/com/myapp/webprj/board/controller/BoardController.java
@@ -0,0 +1,79 @@
+package com.myapp.webprj.board.controller;
+
+import com.myapp.webprj.board.domain.Board;
+import com.myapp.webprj.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board/*")
+@Log4j2 //로그에 시간이 나오게 해줌
+public class BoardController {
+
+    private final BoardService boardService;
+
+    //게시물 목록 요청 처리
+    @GetMapping("/list")
+    public String list(Model model) {
+        log.info("/board/list Get 요청 발생!");
+        List<Board> list = boardService.getList();
+        model.addAttribute("list", list);
+        return "board/list";
+    }
+
+    //게시글 등록 화면 요청 처리
+    @GetMapping("/register")
+    public String register() {
+        log.info("/board/register GET 요청!");
+        return "board/register";
+    }
+
+    //게시글 등록 처리 요청
+    @PostMapping("/register")
+    public String register(Board board, RedirectAttributes ra) {
+        log.info("/board/register POST 요청: " + board);
+        boardService.register(board);
+        ra.addFlashAttribute("msg", "regSuccess");
+        return "redirect:/board/list";
+    }
+
+    //게시글 상세 조회 요청
+    @GetMapping("/get")
+    public String get(Long bno, Model model) {
+        log.info("/board/get GET 요청! : " + bno);
+        Board board = boardService.get(bno);
+        model.addAttribute("board", board);
+        return "board/get";
+    }
+
+    //게시글 수정 화면 요청
+    @GetMapping("/modify")
+    public String modify(Long bno, Model model) {
+        log.info("/board/modify MODIFY 요청!");
+        Board board = boardService.get(bno);
+        model.addAttribute("board", board);
+        return "board/modify";
+    }
+
+    //게시글 수정 완료 처리 요청
+    @PostMapping("/modify")
+    public String modify(Board board, RedirectAttributes ra) {
+        log.info("/board/modify MODIFY 요청 : " + board);
+        boolean modify = boardService.modify(board);
+        if (modify) {
+            ra.addFlashAttribute("msg", "modifySuccess");
+        }
+        return "redirect:/board/list";
+    }
+
+
+}
---
+++ src/main/java/com/myapp/webprj/board/service/BoardService.java
@@ -0,0 +1,27 @@
+package com.myapp.webprj.board.service;
+
+
+import com.myapp.webprj.board.domain.Board;
+
+import java.util.List;
+
+public interface BoardService {
+
+    //레파지토리(여기선 BoardMapper)의 기능과 1:1로 매칭해주는 것이 좋음
+
+    //게시글 등록 과정
+    void register(Board board);
+
+    //게시글 상세 조회 과정
+    Board get(Long bno);
+
+    //게시글 수정 과정
+    boolean modify(Board board);
+
+    //게시글 삭제 과정
+    boolean remove(Long bno);
+
+    //게시물 전체 조회 과정
+    List<Board> getList();
+
+}
