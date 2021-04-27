ea75d72f59823c34e5ef8d9f59705a874052a666
bbongcat <lollolllo328@gmail.com>
2021-04-27 10:58:49 +0900

    [21.04.27] # 회원관리 매퍼, 서비스 작성

---
+++ src/main/java/com/myapp/webprj/member/service/MemberServiceImpl.java
@@ -0,0 +1,49 @@
+package com.myapp.webprj.member.service;
+
+import com.myapp.webprj.member.domain.Member;
+import com.myapp.webprj.member.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberMapper memberMapper;
+
+    @Override
+    public void signUp(Member member) {
+
+        //평문 비번
+        String rawPassword = member.getPassword(); //평문으로 넘어온 pw를 꺼냄
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        //암호화 비번
+        String encodedPassword = encoder.encode(rawPassword); //평문 pw를 암호화
+        member.setPassword(encodedPassword); //암호화된 pw를 저장함
+
+        memberMapper.register(member); //암호화된 pw가 포함된 회원정보를 등록함
+    }
+
+    @Override
+    public boolean isDuplicate(String kind, String info) {
+
+        Map<String, Object> checkDataMap = new HashMap<>();
+        checkDataMap.put("kind", kind);
+        checkDataMap.put("info", info);
+
+        //
+        int resultNumber = memberMapper.isDuplicate(checkDataMap);
+
+        return resultNumber  == 1;
+    }
+
+    @Override
+    public Member getUser(String account) {
+        return memberMapper.getUserInfo(account);
+    }
+}
